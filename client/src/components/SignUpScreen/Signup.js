d3466190e16c5fe9b0a6864081530209ea8c12aa
Nipun Gupta <133570039+guptanipun2018@users.noreply.github.com>
2025-09-23 21:16:37 +0530

    Updated backend url

--- client/src/components/SignUpScreen/Signup.js
+++ client/src/components/SignUpScreen/Signup.js
@@ -18,7 +18,7 @@ const Signup = () => {
   const handleSubmit = async(e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://myapp-backend.onrender.com/api/signup", { 
+      const res = await axios.post("https://booksstore-a617.onrender.com/api/signup", { 
         name, 
         email, 
         password, 
